b2bf5f91669d11809e7237cac2bce6903b6cf2b0
Heike Tineo <tineo.cruz.heike@gmail.com>
2022-03-31 00:17:07 -0500

    Vista Principal
    Se realizó CSS de la vista principal y parte de las funcionalidades de JS.

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,26 @@
  * - Crear la funcion de algoritmo de luhn
  * - Mostrar los 4 
  */
+function validation ()
+{
+    getinformation()
+   
+}
+
+function getinformation()
+{
+const asterisco ="*"
+let number = document.getElementById("card_Number");
+document.getElementById("number").innerHTML= asterisco.repeat(12) + number.value.slice(12,16);
+let expiration = document.getElementById("expiration_Date");
+document.getElementById("expiration").innerHTML= expiration.value;
+let cvv = document.getElementById("cvv_Number")
+document.getElementById("cvv").innerHTML= cvv.value;
+let name_Card = document.getElementById("name_Card");
+let lastname = document.getElementById("lastname_Card");
+document.getElementById("full_Name").innerHTML= name_Card.value +" "+ lastname_Card.value;
+}
+
+
+
+
